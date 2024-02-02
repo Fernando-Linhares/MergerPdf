2bbe04b5d831232d718a70d7937f3ffee0f49156
Fernando Linhares Silvestre <fernandomadethat@gmail.com>
2024-02-01 21:22:43 -0300

    This is a command line pdf merger to a simple way to make merges using csharp and dotnet 7.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace MergePdf;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string outputfile = args[0];
+
+        var pdfs = new List<string>();
+
+        for(var i = 1; i< args.Length; i++)
+        {
+            pdfs.Add(args[i]);
+
+        }
+
+        var record = new PdfRecord(pdfs);
+
+        record.Merged = outputfile;
+
+        record.Merge();
+
+        record.Out();
+    }
+}
---
+++ README.md
@@ -0,0 +1,18 @@
+## PDF MERGER COMMAND LINE
+
+### Overview
+
+This is a command line pdf merger to a simple way to make merges using csharp and dotnet 7.
+
+### pdf merger
+
+usage
+        $ mergepdf /path/to/fileoutput  /path/to/fileinput /path/to/fileinput
+
+run this command will output
+        File merged /path/to/fileoutput
+    
+### Dependencies
+
+- Dotnet 7
+- PDFsharp
